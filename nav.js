186cfffd3e6d1ca53b8685634133b1d11a8aede9
Toheeb-git <addmore12000@gmail.com>
2025-09-18 12:57:31 +0100

    javascript

---
+++ nav.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementsByClassName("nav-bar");
+const button = document.getElementById("btn");
+ const listitem = document.getElementById("list-item");
+
+ button.addEventListener("click", function(){
+    
+ })
